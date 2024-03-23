d5027f5a3ba41979b6694af744c5d562ff025e78
StandingPad Animations <mahidsheikh@pm.me>
2024-03-23 17:28:30 -0500

    Revert "Moved entry point to Kotlin"
    This reverts commit 76db18bf81f2b9173d58b8c1da51bf21a553aa2b.

--- src/org/jmc/ProgressCallback.java
+++ src/org/jmc/ProgressCallback.java
@@ -1,19 +1,21 @@
 package org.jmc;
 
+import javax.annotation.CheckForNull;
+
 /**
  * Simple callback to indicate progress of an operation.
  */
-interface ProgressCallback
+public interface ProgressCallback
 {
 	/**
 	 * Sets the current level of progress.
 	 * @param value Progress, in the interval [0,1]
 	 */
-	fun setProgress(value: Float);
+	public void setProgress(float value);
 
 	/**
 	 * Sets a message to describe currently running task.
 	 * @param message The current message, null for no message.
 	 */
-	fun setMessage(message: String);
+	public void setMessage(@CheckForNull String message);
 }
